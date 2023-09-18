a649490cabea7859d078e784e2790cb08fc9f6ca
liantia2 <tiantian.lian@mail.utoronto.ca>
2023-09-18 10:08:25 -0400

    refactored to extract helper method

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -2,12 +2,12 @@ import java.util.List;
 
 public class DataTypes {
     // TASK 1: fix this code so that it passes the test in DataTypesTest.java
-    public static <T extends Number> long sum(List<T> numbers) {
+    public static long sum(List<Integer> numbers) {
 
         long s = 0;
         // below is a "foreach" loop which iterates through numbers
-        for (T x : numbers) {
-            s += x.longValue();
+        for (int x : numbers) {
+            s += x;
         }
         return s;
     }
