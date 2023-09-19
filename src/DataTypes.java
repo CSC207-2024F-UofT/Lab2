56ce50799258e75bb2c801dc56c59f91709f7b17
Nimrat Grewal <114554275+NimratGrewal@users.noreply.github.com>
2023-09-18 21:29:54 -0400

    edited code

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -3,12 +3,12 @@ import java.util.List;
 public class DataTypes {
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
         }
-        return Long.valueOf(s);
+        return s;
     }
 
 }
