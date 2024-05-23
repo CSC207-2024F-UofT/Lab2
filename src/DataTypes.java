b8789118366093f46d6a119c97fe53fbe60424a6
Kaisheng liu <kaisheng.liu@mail.utoronto.ca>
2024-05-23 17:02:16 -0400

    fixbug

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -4,6 +4,7 @@ public class DataTypes {
 
     public static long sum(List<Integer> numbers) {
 
+
         long s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (long x : numbers) {
